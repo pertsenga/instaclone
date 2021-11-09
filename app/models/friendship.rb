d63094fbe5049f519e5b87c877ea15f0743dd788
Rupert Señga <pertsenga@gmail.com>
2021-11-09 21:54:44 +0800

    Add friends method for User model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,10 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
   validates :user_id, :friend_id, presence: true
+
+  scope :accepted, -> { where.not accepted_at: nil }
+  scope :rejected, -> { where.not rejected_at: nil }
+  scope :pending, -> { where accepted_at: nil, rejected_at: nil }
+
+  scope :of_user, -> (user) { where("user_id = ? OR friend_id = ?", user.id, user.id) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   has_many :photos, dependent: :delete_all
   has_many :comments, dependent: :delete_all
   has_many :friendships, dependent: :delete_all
+
+  def friends
+    User.where(id: friend_ids)
+  end
+
+  def friend_ids
+    Friendship.of_user(self).accepted.pluck(:user_id, :friend_id).flatten.uniq - [self.id]
+  end
 end
